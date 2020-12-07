39fe412202d1885613dadb0ed4016a31b338bf8a
VinhQuangHoang <hoangvinhquang69@gmail.com>
2020-12-07 21:20:35 +0700

    implement admin page

---
+++ AdminLayout/Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdminLayout.Areas.Admin.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminLayout.Areas.Admin.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        [Area("Admin")]
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (model.UserName == "admin" & model.Password == "123456")
+            {
+
+            }
+            return View();
+        }
+    }
+}
