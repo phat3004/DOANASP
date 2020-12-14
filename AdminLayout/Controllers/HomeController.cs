16d3f5aaecb8ca9aff998bbee68e2ccb48dd9b0c
Phát <nhmphat304@gmail.com>
2020-12-15 00:21:29 +0700

    ajax customer view

--- AdminLayout/Controllers/HomeController.cs
+++ AdminLayout/Controllers/HomeController.cs
@@ -22,16 +22,6 @@ namespace AdminLayout.Controllers
         }
         public IActionResult Login(string Email, string Pass)
         {
-            //1. Tạo danh sách danh mục để hiển thị ở giao diện View thông qua DropDownList
-            //var loai = from c in _context.LoaiSP select c;
-            //ViewBag.Loai = new SelectList(loai, "Id", "Name"); // danh sách Loại SP
-
-            //2. Tạo câu truy vấn kết 2 bảng bằng mệnh đề join
-            //var sp = from l in _context.LoaiSP
-            //         join c in _context.SanPham on l.Id equals c.MaLoai
-            //         select new { l.Name, c.ProName, c.MaLoai, c.Loai, c.Img, c.Id };
-            
-            //3. Tìm kiếm chuỗi truy vấn
             if (!String.IsNullOrEmpty(Email) && !String.IsNullOrEmpty(Pass))
             {
                 var custom = from c in _context.Customer
@@ -45,26 +35,11 @@ namespace AdminLayout.Controllers
             {
                 return NotFound();
             }
-            //5. Chuyển đổi kết quả từ var sang danh sách List<Link>
-             //trả về kết quả
         }
-        public IActionResult Index(string? name, string Email, string Pass)
+        public IActionResult Index(string? name)
         {
-            if (!String.IsNullOrEmpty(Email) && !String.IsNullOrEmpty(Pass))
-            {
-                var custom = from c in _context.Customer
-                             where c.Email == Email && c.Password == Pass
-                             select new { c.Name };
-                //var user =  _context.Customer
-                //.FirstOrDefaultAsync(m => m.Email == Email &&  m.Password == Pass);
-                return View("Index", custom);
-            }
-            else
-            {
-                return NotFound();
-            }
-            //ViewBag.USer = name;
-            //return View();
+            ViewBag.USer = name;
+            return View();
         }
 
         public IActionResult Privacy()
