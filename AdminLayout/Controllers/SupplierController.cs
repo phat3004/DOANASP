4289eb3346c133d883cd63a59fb6fac3c25b9918
tidi2200 <dattruong2200@gmail.com>
2020-12-23 19:33:08 +0700

    add notify.js

--- AdminLayout/Controllers/SupplierController.cs
+++ AdminLayout/Controllers/SupplierController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AdminLayout.Areas.Admin.Data;
+using AdminLayout.Areas.Admin.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace AdminLayout.Controllers
 {
@@ -23,6 +26,35 @@ namespace AdminLayout.Controllers
             ViewBag.listCategory = _context.Category.ToList();
             ViewBag.listSupplier = _context.Supplier.ToList();
 
+            var cart = HttpContext.Session.GetString("cart");
+            if (cart != null)
+            {
+                List<Cart> dataCart = JsonConvert.DeserializeObject<List<Cart>>(cart);
+                if (dataCart.Count > 0)
+                {
+                    ViewBag.carts = dataCart;
+                    ViewBag.listProduct = _context.Product.ToList();
+                    ViewBag.listCategory = _context.Category.ToList();
+                    ViewBag.listSupplier = _context.Supplier.ToList();
+                    var list2 = from m in _context.Product select m;
+                    list2 = list2.Where(m => m.SupplierID.Equals(id));
+                    return View(await list2.ToListAsync());
+                }
+                else
+                {
+                    ViewBag.carts = dataCart;
+                    ViewBag.listProduct = _context.Product.ToList();
+                    ViewBag.listCategory = _context.Category.ToList();
+                    ViewBag.listSupplier = _context.Supplier.ToList();
+                    var list1 = from m in _context.Product select m;
+                    list1 = list1.Where(m => m.SupplierID.Equals(id));
+                    return View(await list1.ToListAsync());
+                }
+            }
+            ViewBag.carts = null;
+            ViewBag.listProduct = _context.Product.ToList();
+            ViewBag.listCategory = _context.Category.ToList();
+            ViewBag.listSupplier = _context.Supplier.ToList();
             var list = from m in _context.Product select m;
             list = list.Where(m => m.SupplierID.Equals(id));
             return View(await list.ToListAsync());
